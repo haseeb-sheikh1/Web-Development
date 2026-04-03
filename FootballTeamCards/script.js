669c775166c538175676c0d5bbfc666a9f4a0815
Haseeb Sheikh <haseebsheikh55m@gmail.com>
2026-04-03 18:12:09 +0500

    filtering players by nickname

--- FootballTeamCards/script.js
+++ FootballTeamCards/script.js
@@ -189,10 +189,21 @@ const setPlayerCards = (arr = players) => {
         <div class="player-card">
           <h2>${isCaptain ? "(Captain)" : ""} ${name}</h2>
           <p>Position: ${position}</p>
+          <p>Number: ${number}</p>
+          <p>Nickname: ${nickname !== null ? nickname : "N/A"}</p>
         </div>
       `;
     }
-  );
-
-
+  ).join("");
 };
+playersDropdownList.addEventListener("change", (e) => {
+  playerCards.innerHTML = "";
+
+
+  switch (e.target.value) {
+    case "nickname":
+    setPlayerCards(players.filter((player)=>player.nickname))
+  }
+
+
+});
