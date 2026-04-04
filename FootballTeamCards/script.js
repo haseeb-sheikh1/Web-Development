6e10077151f143a3a3cddd2a1c2ac9f4f4bd5059
Haseeb Sheikh <haseebsheikh55m@gmail.com>
2026-04-04 21:47:29 +0500

    set player by dropdown choices

--- FootballTeamCards/script.js
+++ FootballTeamCards/script.js
@@ -202,7 +202,30 @@ playersDropdownList.addEventListener("change", (e) => {
 
   switch (e.target.value) {
     case "nickname":
-    setPlayerCards(players.filter((player)=>player.nickname))
+    setPlayerCards(players.filter((player)=>player.nickname!==null));
+     break;
+    case "forward":
+      setPlayerCards(players.filter((player) => player.position === "forward"));
+      break;
+    case "midfielder":
+      setPlayerCards(
+        players.filter((player) => player.position === "midfielder")
+      );
+      break;
+    case "defender":
+      setPlayerCards(
+        players.filter((player) => player.position === "defender")
+      );
+      break;
+    case "goalkeeper":
+      setPlayerCards(
+        players.filter((player) => player.position === "goalkeeper")
+      );
+      break;
+
+default:
+   setPlayerCards();
+      break;
   }
 
 
