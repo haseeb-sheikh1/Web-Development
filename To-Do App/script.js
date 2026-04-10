7003108fcb02a34a5995f6fe4802d1da45290280
Haseeb Sheikh <haseebsheikh55m@gmail.com>
2026-04-10 17:44:49 +0500

    feat: implement dynamic task ID generation and index lookup

--- To-Do App/script.js
+++ To-Do App/script.js
@@ -29,6 +29,10 @@ discardBtn.addEventListener("click", () => {
 
 taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
-
+  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+  const taskObj = {
+    id: `${titleInput.value.toLowerCase().split(' ').join('-')}`,  
+  };
+  console.log(taskObj);
 
 });
